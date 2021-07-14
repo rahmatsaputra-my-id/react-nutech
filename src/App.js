e9488f0d344d87e214da70fec6086887debd8ca9
rahmatsaputra-my-id <rahmatsaputra.my.id@gmail.com>
2021-07-14 20:35:18 +0700

    - done create function - done delete function - done read function

--- src/App.js
+++ src/App.js
@@ -1,41 +1,17 @@
 import './App.css';
 import CTable from './Component/CTable';
 import React, { Component } from 'react';
-import { getAllData } from './Helper/ActionGlobal';
 
 export default class App extends Component {
    constructor(props) {
       super(props);
-
-      this.state = {
-         allData: false,
-      }
-   }
-
-   componentDidMount() {
-      this._handlerGetAllData();
-   }
-
-   async _handlerGetAllData() {
-      const readAllData = await getAllData();
-
-      if (readAllData) {
-         this.setState({
-            listAllData: readAllData.data.data
-         });
-         console.log("CTable@_handlerGetAllData", readAllData.data.data);
-      }
    }
 
    render() {
-      const { listAllData } = this.state;
-
       return (
          <div className="App">
-            <h2> PT Nutech Programing Test Rahmat Saputra</h2>
-            <CTable
-               listAllData={listAllData}
-            />
+            <h2>Programing Test Rahmat Saputra</h2>
+            <CTable/>
          </div>
       );
    }
--- src/Helper/ActionGlobal.js
+++ src/Helper/ActionGlobal.js
@@ -1,78 +1,48 @@
-import axios from "axios";
+import Axios from "axios";
 
 
-export const getAllData = async (fullName, email, subject, description, customerName, attachmentToken) => {
-   const baseUrl = "https://staging.rahmatsaputra.my.id"
+// export const getAllData = async () => {
+//    try {
+//       const baseUrl = "https://staging.rahmatsaputra.my.id";
+
+//       const url = baseUrl + "/v0/nutech/product";
+//       const result = await Axios.get(url);
+//       //   return await AxiosAdapter.post(url, body, config);
+//       return result;
+
+//    } catch (error) {
+//       console.log('ActionGlobal.web@postTicketZendeskWidget', error.message);
+//       return false;
+//    }
+// };
+
+export const postData = async (url_to_image, product_name,purchase_price,selling_price,stock) => {
+   // console.log("ActionGlobal.js@postData>Top", url_to_image, product_name, purchase_price, selling_price, stock);
    try {
-      //   const config = {
-      //     headers: {
-      //       "Authorization": "Bearer " + actZendesk
-      //     },
-      //   };
+      const baseUrl = "https://staging.rahmatsaputra.my.id";
+      const config = {
+         headers: {
+            "Content-Type": "multipart/form-data; boundary=MultipartBoundry;"
+         },
+      };
 
-      //   const body = {
-      //     ticket: {
-      //       ticket_form_id: "360003061114" ,
-      //       subject: subject,
-      //       comment: {
-      //         body: description,
-      //         uploads: attachmentToken
-      //       },
-      //       custom_fields: 
-      //         [
-      //           { id: 360036659793, value: fullName },
-      //           { id: 360036659813, value: email },
-      //           { id: 360036659793, value: customerName }
-      //         ]
-      //       ,
-      //       collaborators: [email, { name: fullName, email: email }]
-      //     }
-      //   };
+      var formData = new FormData();
+
+      formData.append("url_to_image", url_to_image);
+      formData.append("product_name", product_name);
+      formData.append("purchase_price", purchase_price);
+      formData.append("selling_price", selling_price);
+      formData.append("stock", stock);
 
       const url = baseUrl + "/v0/nutech/product";
-      const result = await axios.get(url);
-      //   return await AxiosAdapter.post(url, body, config);
-      return result;
+      const result = await Axios.post(url, formData, config);
 
+      return result;
    } catch (error) {
-      console.log('ActionGlobal.web@postTicketZendeskWidget', error.message);
-      return false;
+      console.log('ActionGlobal.js@postData', error.response);
+      return error.response;
    }
 };
-
-//  export const getDataById = async ( fullName, email, subject, description, customerName, attachmentToken) => {
-//    try {
-//      const config = {
-//        headers: {
-//          "Authorization": "Bearer " + actZendesk
-//        },
-//      };
-
-//      const body = {
-//        ticket: {
-//          ticket_form_id: "360003061114" ,
-//          subject: subject,
-//          comment: {
-//            body: description,
-//            uploads: attachmentToken
-//          },
-//          custom_fields: 
-//            [
-//              { id: 360036659793, value: fullName },
-//              { id: 360036659813, value: email },
-//              { id: 360036659793, value: customerName }
-//            ]
-//          ,
-//          collaborators: [email, { name: fullName, email: email }]
-//        }
-//      };
-//      const url = baseUrlZendesk + "api/v2/tickets.json";
-//      return await AxiosAdapter.post(url, body, config);
-//    } catch (error) {
-//      console.log('ActionGlobal.web@postTicketZendeskWidget', error.message);
-//      return false;
-//    }
-//  };
 
 //  export const updateData = async ( fullName, email, subject, description, customerName, attachmentToken) => {
 //    try {
@@ -108,37 +78,15 @@ export const getAllData = async (fullName, email, subject, description, customer
 //    }
 //  };
 
-// export const postData = async ( fullName, email, subject, description, customerName, attachmentToken) => {
-//    try {
-//      const config = {
-//        headers: {
-//          "Authorization": "Bearer " + actZendesk
-//        },
-//      };
+export const deleteData = async (id) => {
 
-//      const body = {
-//        ticket: {
-//          ticket_form_id: "360003061114" ,
-//          subject: subject,
-//          comment: {
-//            body: description,
-//            uploads: attachmentToken
-//          },
-//          custom_fields: 
-//            [
-//              { id: 360036659793, value: fullName },
-//              { id: 360036659813, value: email },
-//              { id: 360036659793, value: customerName }
-//            ]
-//          ,
-//          collaborators: [email, { name: fullName, email: email }]
-//        }
-//      };
-//      const url = baseUrlZendesk + "api/v2/tickets.json";
+   try {
+      const baseUrl = "https://staging.rahmatsaputra.my.id";
+      const url = baseUrl + "/v0/nutech/product/" + id;
 
-//      return await AxiosAdapter.post(url, body, config);
-//    } catch (error) {
-//      console.log('ActionGlobal.web@postData', error.message);
-//      return false;
-//    }
-//  };
+      return await Axios.delete(url);
+   } catch (error) {
+      console.log('ActionGlobal.web@updateData', error.response);
+      return error.response;
+   }
+};
